71463aa8ebcd1632f51fc4cac9db3862f394c513
Sagar Singh <sagarsingh7744@gmail.com>
2025-02-20 19:43:41 +0530

    Day 014 Completed

---
+++ Front-End/Day 014/script.js
@@ -0,0 +1,47 @@
+console.log('Hello JS');
+console.info('Info Message');
+console.warn('Warning Message!');
+console.error('Error Message!');
+
+// let username = prompt("Enter your name: ");
+
+// alert("Hello welcome to JS")
+
+// let response = confirm('Are you sure?');
+
+// var Name = 'john';
+
+// Name = 'Sam';
+
+// console.log(Name);
+
+// const xcountry = 'India';
+
+// xcountry = 'Pakistan'
+
+// console.log(xcountry);
+
+
+// let score = 100;
+// score = 150
+// score = 200
+// console.log(score);
+
+let a = 5
+console.log(a++); // 5
+console.log(++a); // 6 || 7
+
+
+
+// let num2 = 10
+
+// console.log(num1 + num2);
+// console.log(num1 % num2);
+
+// let count = 1;
+// count++
+// console.log(count);
+// console.log(count);
+
+
+
